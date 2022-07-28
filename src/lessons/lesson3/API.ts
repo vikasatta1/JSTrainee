d6412edc172c65d244dbf872d4685da78d60d560
Vika <Vikasatta1998@gmail.com>
2022-07-28 16:27:28 +0300

    3

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
-
+const key = 'bd9500c8';
 const configOMB = {
-    baseURL: 'http://www.omdbapi.com',
+    baseURL: `http://www.omdbapi.com/apikey=${key}/`,
 };
-const key = '';
+
 const axiosInstance = axios.create(configOMB);
 
 const API = {
     searchFilmsByTitle: (title: string) => {
+      // @ts-ignore
+        return axiosInstance.get('', {t: title})
     },
     searchFilmsByType: (title: string, type: string) => {
     }
